6ae8cb5ffb534c5e33772c4f3c34ff31798095da
Samuel Slotsky <sam.slotsky@reachlocal.com>
2016-07-29 11:54:55 -0500

    switch to es5

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import Immutable, { List, Stack, Map, OrderedMap, Set, OrderedSet, Record } from 'immutable'
+var Immutable = require('immutable')
 
-export function postStart(context) {
+exports.postStart = function(context) {
   const repl = context.repl
 
   repl.context.Immutable = Immutable
-  repl.context.List = List
-  repl.context.Stack = Stack
-  repl.context.Map = Map
-  repl.context.OrderedMap = OrderedMap
-  repl.context.Set = Set
-  repl.context.OrderedSet = OrderedSet
-  repl.context.Record = Record
+  repl.context.List = Immutable.List
+  repl.context.Stack = Immutable.Stack
+  repl.context.Map = Immutable.Map
+  repl.context.OrderedMap = Immutable.OrderedMap
+  repl.context.Set = Immutable.Set
+  repl.context.OrderedSet = Immutable.OrderedSet
+  repl.context.Record = Immutable.Record
 }
